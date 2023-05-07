9072330d80f664d2232e03d9c0a0d2b1198e09aa
Wei Yang <wei.yang@rwth-aachen.de>
2023-05-07 16:57:43 +0200

    Init Demo Project with vite.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # dt-bim-xr
 Digital Twins Prototyping for BIM with WebXR.
+
+
+# Development
+
+```sh
+npm i
+cd demo
+npm i
+npm run dev
+
+```
+
+# Build
+```sh
+cd demo
+npm run build
+```
+
+# Debug Inspector
+change the condition to **true**, and press **clt+shift+I**.
+
+```js
+// hide/show the Inspector
+  if (true) {
+    window.addEventListener("keydown", async (ev) => {
+      // Shift+Ctrl+I
+      if (ev.shiftKey && ev.ctrlKey && ev.code === "KeyI") {
+        await import("@babylonjs/core/Debug/debugLayer");
+        await import("@babylonjs/inspector");
+        if (scene.debugLayer.isVisible()) {
+          scene.debugLayer.hide();
+        } else {
+          scene.debugLayer.show();
+        }
+      }
+    });
+  }
+```
+
+change position of MainCanvas in style.css to **relative** to see the scene explorer.
+
+```css
+#MainCanvas {
+  position: relative; left: 0; top: 0; width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0);
+}
+```
---
+++ demo/src/main.ts
@@ -0,0 +1,130 @@
+import './style.css'
+import {
+  Engine,
+  FreeCamera,
+  HemisphericLight,
+  MeshBuilder,
+  Scene,
+  Vector3,
+  WebXRSessionManager
+} from "@babylonjs/core";
+
+//#region WebXRPolyfill
+
+declare global {
+  interface Window {
+    WebXRPolyfill?: any;
+  }
+  class WebXRPolyfill { }
+}
+
+const xrPolyfillPromise = new Promise<void>((resolve) => {
+  if (navigator.xr) {
+    return resolve();
+  }
+
+  if (window.WebXRPolyfill) {
+    new WebXRPolyfill();
+    return resolve();
+  } else {
+    const url = "https://cdn.jsdelivr.net/npm/webxr-polyfill@latest/build/webxr-polyfill.js";
+    const s = document.createElement("script");
+    s.src = url;
+    document.head.appendChild(s);
+    s.onload = () => {
+      new WebXRPolyfill();
+      resolve();
+    };
+  }
+});
+
+//#endregion
+
+(async () => {
+
+  // create the canvas html element and attach it to the webpage
+  const canvas = <HTMLCanvasElement>document.getElementById("MainCanvas");
+
+  //#region Setup engine and scene
+
+  // wait for the polyfill to kick in
+  await xrPolyfillPromise;
+  //console.log(navigator.xr); // should be there!
+  console.log("immersive-vr supported?", await WebXRSessionManager.IsSessionSupportedAsync("immersive-vr")); // should be true
+
+  const engine = new Engine(canvas, true);
+  // This creates a basic Babylon Scene object (non-mesh)
+  const scene = new Scene(engine);
+
+  window.addEventListener('resize', function () {
+    engine.resize()
+  })
+
+  // run the main render loop
+  engine.runRenderLoop(() => {
+    if (scene.activeCamera) {
+      scene.render()
+    }
+  })
+
+  //#endregion
+
+  //#region Setup scene
+
+  // This creates and positions a free camera (non-mesh)
+  let camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+  // This targets the camera to scene origin
+  camera.setTarget(Vector3.Zero());
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+  let light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.7;
+
+  // Our built-in 'sphere' shape. Params: name, options, scene
+  let sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+
+  // Move the sphere upward 1/2 its height
+  sphere.position.y = 1;
+
+  // Our built-in 'ground' shape. Params: name, options, scene
+  let ground = MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+
+  //#endregion
+
+  //const env = scene.createDefaultEnvironment();
+
+  //#region Setup WebXR
+
+  await scene.createDefaultXRExperienceAsync({
+    floorMeshes: [ground]
+  });
+
+  //#endregion
+
+  //#region Debug Inspector
+
+  // hide/show the Inspector
+  if (false) {
+    window.addEventListener("keydown", async (ev) => {
+      // Shift+Ctrl+I
+      if (ev.shiftKey && ev.ctrlKey && ev.code === "KeyI") {
+        await import("@babylonjs/core/Debug/debugLayer");
+        await import("@babylonjs/inspector");
+        if (scene.debugLayer.isVisible()) {
+          scene.debugLayer.hide();
+        } else {
+          scene.debugLayer.show();
+        }
+      }
+    });
+  }
+
+  //#endregion
+
+})();
