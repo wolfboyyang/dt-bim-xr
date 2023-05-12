b8ed1239868ed83fcedef1a8d908fc9e0a24c9a4
Wei Yang <wei.yang@rwth-aachen.de>
2023-05-12 14:30:23 +0200

    Getting Started - Chapter 2 - Face Materials

--- village/src/main.ts
+++ village/src/main.ts
@@ -10,6 +10,7 @@ import {
   StandardMaterial,
   Color3,
   Texture,
+  Vector4,
 } from "@babylonjs/core";
 
 (async () => {
@@ -43,15 +44,33 @@ import {
   //color
   const groundMat = new StandardMaterial("groundMat");
   groundMat.diffuseColor = new Color3(0, 1, 0)
-  
+
   //texture
   const roofMat = new StandardMaterial("roofMat");
   roofMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/roof.jpg");
   const boxMat = new StandardMaterial("boxMat");
-  boxMat.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/floor.png")
+  boxMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/cubehouse.png")
+  const semiBoxMat = new StandardMaterial("boxMat");
+  semiBoxMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/semihouse.png")
+
+  //options parameter to set different images on each side
+  let faceUV = [
+    new Vector4(0.5, 0.0, 0.75, 1.0), //rear face
+    new Vector4(0.0, 0.0, 0.25, 1.0), //front face
+    new Vector4(0.25, 0, 0.5, 1.0), //right side
+    new Vector4(0.75, 0, 1.0, 1.0), //left side
+    // top 4 and bottom 5 not seen so not set
+  ];
+  let semiFaceUV = [
+    new Vector4(0.6, 0.0, 1.0, 1.0), //rear face
+    new Vector4(0.0, 0.0, 0.4, 1.0), //front face
+    new Vector4(0.4, 0, 0.6, 1.0), //right side
+    new Vector4(0.4, 0, 0.6, 1.0), //left side
+    // top 4 and bottom 5 not seen so not set
+  ];
 
   /**** World Objects *****/
-  const box = MeshBuilder.CreateBox("box", {});
+  const box = MeshBuilder.CreateBox("box", { faceUV: faceUV, wrap: true });
   box.position.y = 0.5;
   box.material = boxMat;
   const roof = MeshBuilder.CreateCylinder("roof", { diameter: 1.3, height: 1.2, tessellation: 3 });
@@ -59,6 +78,18 @@ import {
   roof.scaling.x = 0.75;
   roof.rotation.z = Math.PI / 2;
   roof.position.y = 1.22;
+
+  const semiBox = MeshBuilder.CreateBox("box", { width: 2, faceUV: semiFaceUV, wrap: true });
+  semiBox.position.x = 2;
+  semiBox.position.y = 0.5;
+  semiBox.material = semiBoxMat;
+  const semiRoof = MeshBuilder.CreateCylinder("roof", { diameter: 1.3, height: 1.2, tessellation: 3 });
+  semiRoof.material = roofMat;
+  semiRoof.scaling.x = 0.75;
+  semiRoof.rotation.z = Math.PI / 2;
+  semiRoof.scaling.y = 2;
+  semiRoof.position.x = 2;
+  semiRoof.position.y = 1.22;
 
   const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 });
   ground.material = groundMat;
