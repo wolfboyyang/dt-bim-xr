79ce52bddc361a300c1425f4a846e5d0ec901404
Wei Yang <wei.yang@rwth-aachen.de>
2023-05-12 14:11:37 +0200

    Getting Started - Chapter 2 - Texture

--- village/src/main.ts
+++ village/src/main.ts
@@ -7,6 +7,9 @@ import {
   Vector3,
   ArcRotateCamera,
   Sound,
+  StandardMaterial,
+  Color3,
+  Texture,
 } from "@babylonjs/core";
 
 (async () => {
@@ -36,14 +39,29 @@ import {
   camera.attachControl(canvas, true);
   const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
+  /**** Materials *****/
+  //color
+  const groundMat = new StandardMaterial("groundMat");
+  groundMat.diffuseColor = new Color3(0, 1, 0)
+  
+  //texture
+  const roofMat = new StandardMaterial("roofMat");
+  roofMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/roof.jpg");
+  const boxMat = new StandardMaterial("boxMat");
+  boxMat.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/floor.png")
+
+  /**** World Objects *****/
   const box = MeshBuilder.CreateBox("box", {});
   box.position.y = 0.5;
-  const roof = MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tessellation: 3});
+  box.material = boxMat;
+  const roof = MeshBuilder.CreateCylinder("roof", { diameter: 1.3, height: 1.2, tessellation: 3 });
+  roof.material = roofMat;
   roof.scaling.x = 0.75;
   roof.rotation.z = Math.PI / 2;
   roof.position.y = 1.22;
 
   const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 });
+  ground.material = groundMat;
 
   // try sample sound from https://www.soundhelix.com/examples/mp3/SoundHelix-Song-17.mp3
   //const sound = new Sound("name", "SoundHelix-Song-17.mp3", scene, null, { loop: true, autoplay: true });
