007d9badf990f13d161be768ad47a92f690fd464
Wei Yang <wei.yang@rwth-aachen.de>
2023-05-13 18:16:07 +0200

    Chapter 3 - A Walk Around The Village

--- village/src/main.ts
+++ village/src/main.ts
@@ -16,6 +16,9 @@ import {
   Mesh,
   InstancedMesh,
   SceneLoader,
+  Axis,
+  Tools,
+  Space,
 } from "@babylonjs/core";
 import "@babylonjs/loaders/glTF";
 
@@ -47,47 +50,56 @@ import "@babylonjs/loaders/glTF";
   const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
   SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "village.glb");
-  SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "car.glb").then(() => {
-    const car = scene.getMeshByName("car")!;
-    car.rotation = new Vector3(Math.PI / 2, 0, -Math.PI / 2);
-    car.position.y = 0.16;
-    car.position.x = -3;
-    car.position.z = 8;
 
-    //car animation
-    const animCar = new Animation("carAnimation", "position.z", 30, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
+  const walk = function (this, turn, dist) {
+    this.turn = turn;
+    this.dist = dist;
+  }
 
-    const carKeys = [
-      {
-        frame: 0,
-        value: 8
-      },
-      {
-        frame: 150,
-        value: -7
-      },
-      {
-        frame: 200,
-        value: -7
-      },
-    ];
+  const track = [
+    new walk(86, 7),
+    new walk(-85, 14.8),
+    new walk(-93, 16.5),
+    new walk(48, 25.5),
+    new walk(-112, 30.5),
+    new walk(-72, 33.2),
+    new walk(42, 37.5),
+    new walk(-98, 45.2),
+    new walk(0, 47),
+  ];
 
-    animCar.setKeys(carKeys);
+  // Dude
+  SceneLoader.ImportMeshAsync("him", "/scenes/Dude/", "Dude.babylon", scene).then((result) => {
+    var dude = result.meshes[0];
+    dude.scaling = new Vector3(0.008, 0.008, 0.008);
 
-    car.animations = [animCar];
+    dude.position = new Vector3(-6, 0, 0);
+    dude.rotate(Axis.Y, Tools.ToRadians(-95), Space.LOCAL);
+    const startRotation = dude.rotationQuaternion!.clone();
 
-    scene.beginAnimation(car, 0, 200, true);
+    scene.beginAnimation(result.skeletons[0], 0, 100, true, 1.0);
 
-    //wheel animation
-    const wheelRB = scene.getMeshByName("wheelRB");
-    const wheelRF = scene.getMeshByName("wheelRF");
-    const wheelLB = scene.getMeshByName("wheelLB");
-    const wheelLF = scene.getMeshByName("wheelLF");
-    
-    scene.beginAnimation(wheelRB, 0, 30, true);
-    scene.beginAnimation(wheelRF, 0, 30, true);
-    scene.beginAnimation(wheelLB, 0, 30, true);
-    scene.beginAnimation(wheelLF, 0, 30, true);
+    let distance = 0;
+    let step = 0.015;
+    let p = 0;
+
+    scene.onBeforeRenderObservable.add(() => {
+      dude.movePOV(0, 0, step);
+      distance += step;
+
+      if (distance > track[p].dist) {
+
+        dude.rotate(Axis.Y, Tools.ToRadians(track[p].turn), Space.LOCAL);
+        p += 1;
+        p %= track.length;
+        if (p === 0) {
+          distance = 0;
+          dude.position = new Vector3(-6, 0, 0);
+          dude.rotationQuaternion = startRotation.clone();
+        }
+      }
+
+    })
   });
 
   //#endregion
