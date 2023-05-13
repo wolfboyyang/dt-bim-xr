923abf7a6b9b6fddd4adcde810b1c0e06578b62d
Wei Yang <wei.yang@rwth-aachen.de>
2023-05-13 17:19:49 +0200

    Getting Started - Chapter 3 - Buiding the car

--- village/src/main.ts
+++ village/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import {
   Engine,
   HemisphericLight,
+  PolygonMeshBuilder,
   MeshBuilder,
   Scene,
   Vector3,
@@ -15,7 +16,7 @@ import {
   InstancedMesh,
   SceneLoader,
 } from "@babylonjs/core";
-import "@babylonjs/loaders/glTF";
+import earcut from "earcut";
 
 (async () => {
 
@@ -44,8 +45,46 @@ import "@babylonjs/loaders/glTF";
   camera.attachControl(canvas, true);
   const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
-  SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "village.glb");
+  const car = buildCar();
 
   //#endregion
 
 })();
+
+function buildCar() {
+  //base
+  const outline = [
+    new Vector3(-0.3, 0, -0.1),
+    new Vector3(0.2, 0, -0.1),
+  ]
+
+  //curved front
+  for (let i = 0; i < 20; i++) {
+    outline.push(new Vector3(0.2 * Math.cos(i * Math.PI / 40), 0, 0.2 * Math.sin(i * Math.PI / 40) - 0.1));
+  }
+
+  //top
+  outline.push(new Vector3(0, 0, 0.1));
+  outline.push(new Vector3(-0.3, 0, 0.1));
+
+  //back formed automatically
+
+  const car = MeshBuilder.ExtrudePolygon("car", { shape: outline, depth: 0.2 }, undefined, earcut);
+  
+  const wheelRB = MeshBuilder.CreateCylinder("wheelRB", { diameter: 0.125, height: 0.05 })
+  wheelRB.parent = car;
+  wheelRB.position.z = -0.1;
+  wheelRB.position.x = -0.2;
+  wheelRB.position.y = 0.035;
+
+  const wheelRF = wheelRB.clone("wheelRF");
+  wheelRF.position.x = 0.1;
+
+  const wheelLB = wheelRB.clone("wheelLB");
+  wheelLB.position.y = -0.2 - 0.035;
+
+  const wheelLF = wheelRF.clone("wheelLF");
+  wheelLF.position.y = -0.2 - 0.035;
+
+  return car;
+}
