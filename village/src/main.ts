20d27d051535f0de0cc80f91823f3a64aa12dd67
Wei Yang <wei.yang@rwth-aachen.de>
2023-05-13 17:38:13 +0200

    Load car from a scene file.

--- village/src/main.ts
+++ village/src/main.ts
@@ -46,98 +46,18 @@ import earcut from "earcut";
   camera.attachControl(canvas, true);
   const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
-  const car = buildCar(scene);
-  car.rotation.x = -Math.PI / 2;
+  SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "car.babylon").then(() => {
+    const wheelRB = scene.getMeshByName("wheelRB");
+    const wheelRF = scene.getMeshByName("wheelRF");
+    const wheelLB = scene.getMeshByName("wheelLB");
+    const wheelLF = scene.getMeshByName("wheelLF");
+    console.log(wheelRB!.animations)
+    scene.beginAnimation(wheelRB, 0, 30, true);
+    scene.beginAnimation(wheelRF, 0, 30, true);
+    scene.beginAnimation(wheelLB, 0, 30, true);
+    scene.beginAnimation(wheelLF, 0, 30, true);
+  });
 
   //#endregion
 
 })();
-
-function buildCar(scene: Scene) {
-  //base
-  const outline = [
-    new Vector3(-0.3, 0, -0.1),
-    new Vector3(0.2, 0, -0.1),
-  ]
-
-  //curved front
-  for (let i = 0; i < 20; i++) {
-    outline.push(new Vector3(0.2 * Math.cos(i * Math.PI / 40), 0, 0.2 * Math.sin(i * Math.PI / 40) - 0.1));
-  }
-
-  //top
-  outline.push(new Vector3(0, 0, 0.1));
-  outline.push(new Vector3(-0.3, 0, 0.1));
-
-  //back formed automatically
-
-  //car face UVs
-  const faceUV = [
-    new Vector4(0, 0.5, 0.38, 1),
-    new Vector4(0, 0, 1, 0.5),
-    new Vector4(0.38, 1, 0, 0.5),
-  ];
-
-  //car material
-  const carMat = new StandardMaterial("carMat");
-  carMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/car.png");
-
-  const car = MeshBuilder.ExtrudePolygon("car", { shape: outline, depth: 0.2, faceUV, wrap: true}, undefined, earcut);
-  car.material = carMat;
-
-  //wheel face UVs
-  const wheelUV = [
-    new Vector4(0, 0, 1, 1),
-    new Vector4(0, 0.5, 0, 0.5),
-    new Vector4(0, 0, 1, 1),
-  ];
-  
-  //wheel material
-  const wheelMat = new StandardMaterial("wheelMat");
-  wheelMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/wheel.png");
-
-  const wheelRB = MeshBuilder.CreateCylinder("wheelRB", { diameter: 0.125, height: 0.05, faceUV: wheelUV})
-  wheelRB.material = wheelMat;
-  wheelRB.parent = car;
-  wheelRB.position.z = -0.1;
-  wheelRB.position.x = -0.2;
-  wheelRB.position.y = 0.035;
-
-  //Animate the Wheels
-  const animWheel = new Animation("wheelAnimation", "rotation.y", 30, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
-
-  const wheelKeys = [
-    //At the animation key 0, the value of rotation.y is 0
-    {
-      frame: 0,
-      value: 0
-    },
-    //At the animation key 30, (after 1 sec since animation fps = 30) the value of rotation.y is 2PI for a complete rotation
-    {
-      frame: 30,
-      value: 2 * Math.PI
-    }
-  ]; 
-
-  //set the keys
-  animWheel.setKeys(wheelKeys);
-
-  //Link this animation to a wheel
-  wheelRB.animations = [animWheel];
-
-  const wheelRF = wheelRB.clone("wheelRF");
-  wheelRF.position.x = 0.1;
-
-  const wheelLB = wheelRB.clone("wheelLB");
-  wheelLB.position.y = -0.2 - 0.035;
-
-  const wheelLF = wheelRF.clone("wheelLF");
-  wheelLF.position.y = -0.2 - 0.035;
-
-  scene.beginAnimation(wheelRB, 0, 30, true);
-  scene.beginAnimation(wheelRF, 0, 30, true);
-  scene.beginAnimation(wheelLB, 0, 30, true);
-  scene.beginAnimation(wheelLF, 0, 30, true);
-
-  return car;
-}
