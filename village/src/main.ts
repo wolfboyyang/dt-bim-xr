fecb267335f04cb180335bf3d0bd4d301911bab8
Wei Yang <wei.yang@rwth-aachen.de>
2023-05-14 12:54:54 +0200

    Chapter 7 - Day to Night

--- village/src/main.ts
+++ village/src/main.ts
@@ -29,6 +29,13 @@ import {
   BackgroundMaterial,
 } from "@babylonjs/core";
 import "@babylonjs/loaders/glTF";
+import {
+  AdvancedDynamicTexture,
+  Control,
+  Slider,
+  StackPanel,
+  TextBlock,
+} from '@babylonjs/gui';
 
 (async () => {
 
@@ -56,7 +63,38 @@ import "@babylonjs/loaders/glTF";
   const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 0, 0));
   camera.attachControl(canvas, true);
   const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
-  light.intensity = 0.1;
+
+  // GUI
+  const adt = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+  const panel = new StackPanel();
+  panel.width = "220px";
+  panel.top = "-25px";
+  panel.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
+  panel.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+  adt.addControl(panel);
+
+  const header = new TextBlock();
+  header.text = "Night to Day";
+  header.height = "30px";
+  header.color = "white";
+  panel.addControl(header); 
+
+  const slider = new Slider();
+  slider.minimum = 0;
+  slider.maximum = 1;
+  slider.borderColor = "black";
+  slider.color = "gray";
+  slider.background = "white";
+  slider.value = 1;
+  slider.height = "20px";
+  slider.width = "200px";
+  slider.onValueChangedObservable.add((value) => {
+      if (light) {
+          light.intensity = value;
+      }
+  });
+  panel.addControl(slider);
 
   const spriteManagerUFO = new SpriteManager("UFOManager", "https://assets.babylonjs.com/environments/ufo.png", 1, { width: 128, height: 76 }, scene);
   const ufo = new Sprite("ufo", spriteManagerUFO);
